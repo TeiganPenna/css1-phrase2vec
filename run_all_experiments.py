899954b369dc4041acc7ac875d6dc4494a9bee3a
Teigan Penna <teigan.penna@gmail.com>
2020-05-30 21:05:36 +1000

    experiment 3 should only run with the phrase vectors

--- run_all_experiments.py
+++ run_all_experiments.py
@@ -18,13 +18,6 @@ word_vectors = p2v.get_token_vectors_from_xlsx(Path('data/3.Filtered_word_vector
 entry_phrases = [re.sub(r"[{}]+".format(punctuation), '', line.strip('\n').lower()) for line in open(Path('data/Train_Set_Entry_Terms.txt'), 'r', encoding='UTF-8').readlines()]
 MeSH_phrases = [re.sub(r"[{}]+".format(punctuation), '', line.strip('\n').lower()) for line in open(Path('data/Train_Set_MeSH_Terms.txt'), 'r', encoding='UTF-8').readlines()]
 
-# use this to create the data for experiment 3
-#combined_phrases = entry_phrases + MeSH_phrases
-#p2v.train_phrase_model_to_xlsx(Path('data/1.Raw Input.txt'), Path('data/experiment3_vectors.xlsx'), combined_phrases)
-
-e3_vectors = p2v.get_token_vectors_from_xlsx(Path('data/experiment3_vectors.xlsx'))
-corpus_entry_phrases, corpus_MeSH_phrases = p2v.get_phrases_in_corpus(e3_vectors, entry_phrases, MeSH_phrases)
-
 phrase_vectors = p2v.get_phrase_vectors_from_raw_phrases(word_vectors, entry_phrases, MeSH_phrases)
 experiments.e1.cosine.run(phrase_vectors, entry_phrases, MeSH_phrases)
 experiments.e1.euclidean.run(phrase_vectors, entry_phrases, MeSH_phrases)
@@ -36,7 +29,15 @@ experiments.e2.cosine.run(weighted_phrase_vectors, entry_phrases, MeSH_phrases)
 experiments.e2.euclidean.run(weighted_phrase_vectors, entry_phrases, MeSH_phrases)
 experiments.e2.sklearn.run(weighted_phrase_vectors, entry_phrases, MeSH_phrases)
 
-experiments.e3.cosine.run(e3_vectors, corpus_entry_phrases, corpus_MeSH_phrases)
-experiments.e3.euclidean.run(e3_vectors, corpus_entry_phrases, corpus_MeSH_phrases)
-experiments.e3.sklearn.run(e3_vectors, corpus_entry_phrases, corpus_MeSH_phrases)
+# use this to create the data for experiment 3
+#p2v.train_phrase_model_to_xlsx(Path('data/1.Raw Input.txt'), Path('data/experiment3_vectors.xlsx'), combined_phrases)
+
+e3_vectors = p2v.get_token_vectors_from_xlsx(Path('data/experiment3_vectors.xlsx'))
+corpus_entry_phrases, corpus_MeSH_phrases = p2v.get_phrases_in_corpus(e3_vectors, entry_phrases, MeSH_phrases)
+corpus_combined_phrases = corpus_entry_phrases + corpus_MeSH_phrases
+e3_phrase_vectors = {phrase_token: e3_vectors[phrase_token] for phrase_token in [p2v.phrase_to_token(phrase) for phrase in corpus_combined_phrases]}
+
+experiments.e3.cosine.run(e3_phrase_vectors, corpus_entry_phrases, corpus_MeSH_phrases)
+experiments.e3.euclidean.run(e3_phrase_vectors, corpus_entry_phrases, corpus_MeSH_phrases)
+experiments.e3.sklearn.run(e3_phrase_vectors, corpus_entry_phrases, corpus_MeSH_phrases)
 
